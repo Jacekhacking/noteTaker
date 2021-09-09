8302fc344093e09202e9bccd4acf09d3f0111d64
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-09-08 18:54:15 -0600

    port fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require("./routes")
 
 
 const app = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
